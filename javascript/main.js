03380bce7472ea54dcc5d729fac4efc2817ab0fb
Paige Lo <sk8asd123@gmail.com>
2018-02-08 19:11:47 -0600

    game push

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,72 @@
-// set random max health so game is not static
-function randomHealth() {
-    var max_health = 100;
-    return Math.floor(Math.random()*max_health) + 1;
-}
+var crystal = {
+    first:
+        {
+            name: "first",
+            value: 0
+        },
+    second:
+        {
+            name: "second",
+            value: 0
+        },
+    third:
+        {
+            name: "third",
+            value: 0
+        }
+};
 
-var characterOne = {name: "one", health_points: "100", attack_power: "1", counter_attack_power: "2"};
-var characterTwo = {name: "two", health_points: "100", attack_power: "1", counter_attack_power: "2"};
-var characterThree = {name: "three", health_points: "100", attack_power: "1", counter_attack_power: "2"};
-var characterFour = {name: "four", health_points: "100", attack_power: "1", counter_attack_power: "2"};
+// Scores (Current and Target)
+var currentScore = 0;
+var targetScore = 0;
 
-function Player(type) {
-    this.type = type;
-}
 
-var attackingPlayer = new Player("attack");
-var defendingPlayer = new Player("defend");
 
-//setup action handlers
+var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Starts the Game (and restarts the game)
+var startGame = function() {
+    currentScore = 0;
+    targetScore = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+
+    crystal.first.value = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    crystal.second.value = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    crystal.third.value = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+
+    $("#your-score").html(currentScore);
+    $("#guess-score").html(targetScore);
+};
+
+var check = function() {
+    if (currentScore > targetScore) {
+        alert("Game Over!");
+        startGame();
+    }
+
+    else if (currentScore === targetScore) {
+        alert("You Win");
+        startGame();
+    }
+};
+
+var addValues = function(clickedCrystal) {
+    currentScore += clickedCrystal.value;
+    $("#your-score").html(currentScore);
+    check();
+};
+
+startGame();
+
+$("#first").click(function() {
+    addValues(crystal.first);
+});
+
+$("#second").click(function() {
+    addValues(crystal.second);
+});
+
+$("#third").click(function() {
+    addValues(crystal.third);
+});
